4bf175f5e21b6eeb418c26fbe4582a91bcb3dd35
Wind-Wing <windwing.me@gmail.com>
2020-12-09 21:09:34 +0800

    Refactor network using keras

--- network.py
+++ network.py
@@ -1,3 +1,4 @@
+import time
 import tensorflow as tf
 import constants
 from data import DataGenerator
@@ -8,14 +9,17 @@ class RNN(object):
         self._build_model()
 
     def _build_model(self):
-        self.data_generator = DataGenerator(constants.training_batch_size)
-
         """ Basic RNN: output = new_state = act(W_hi * input + W_hh * state + B) """
         # TODO: Try GRU, LSTM
         # TODO: Add noise
         # TODO: Momenta update states
         # TODO: Dropout
-        # TODO: active function
+        # TODO: Active function
+        # TODO: Regularization
+
+        self.ckpt_prefix = "./ckpts/checkpoint-"
+        self.log_path = "./logs/"
+        self.data_generator = DataGenerator(constants.training_batch_size)
 
         x = tf.keras.layers.Input(shape=self.data_generator.get_inputs_shape(), batch_size=constants.training_batch_size)
         self.state_sequences = tf.keras.layers.SimpleRNN(
@@ -23,44 +27,30 @@ class RNN(object):
             return_sequences=True)(x)
         y = tf.keras.layers.Dense(1)(self.state_sequences)
 
-        print(x.shape, self.state_sequences.shape, y.shape)
-
-        self.model = tf.keras.Model(x, y)
+        self.model = tf.keras.Model(input=x, output=y)
         self.model.summary()
 
-        self.optimizer = tf.optimizers.Adam(lr=constants.training_batch_size, decay=constants.learning_rate_decay)
-        self.saver = tf.train.Checkpoint(model=self.model, optimizer=self.optimizer)
-        self.ckpt_prefix = "./ckpts/checkpoint-"
+        self.model.compile(
+            optimizer=tf.optimizers.Adam(lr=constants.training_batch_size, decay=constants.learning_rate_decay),
+            loss=tf.keras.losses.mse()
+        )
 
-    def _calc_loss(self, pred_list, gt_list):
-        # TODO: Regularizer
-        return tf.losses.mse(pred_list, gt_list)
-        # return tf.math.reduce_mean(tf.losses.mse(pred_list, gt_list), axis=1, keepdims=True)
-
-    # Return: grads and vars
-    def _build_grads(self, input_list, gt_list):
-        trainable_vars = self.model.trainable_variables
-        with tf.GradientTape() as tape:
-            pred = self.model(input_list)
-            loss = self._calc_loss(pred, gt_list)
-        grads = tape.gradient(loss, trainable_vars)
-        return loss, zip(grads, trainable_vars)
-
-    def train_one_iteration(self):
-        input_list, gt_list = self.data_generator.next_batch()
-        loss, grads_and_vars = self._build_grads(input_list, gt_list)
-        self.optimizer.apply_gradients(grads_and_vars)
-        return loss
+    def train(self):
+        self.model.fit(
+            x=self.data_generator.batch_generator(),
+            epochs=constants.num_epochs,
+            verbose=2,
+            workers=4,
+            use_multiprocessing=True,
+            callbacks=[tf.keras.callbacks.TensorBoard(log_dir=self.log_path)]
+        )
 
     def save(self, epoch):
-        self.saver.save(self.ckpt_prefix + str(epoch))
+        self.model.save(self.ckpt_prefix + str(epoch))
 
     def load(self):
-        self.saver.load(tf.train.latest_checkpoint(self.ckpt_prefix))
+        self.model.load(tf.train.latest_checkpoint(self.ckpt_prefix))
 
     def evaluate(self):
         pass
-
-    def get_learning_rate(self):
-        return self.model.optimizer.lr
 
--- train.py
+++ train.py
@@ -1,30 +1,18 @@
-import tensorflow as tf
-import numpy as np
 from network import RNN
-import constants
+import pickle
 
 
 def train():
+    # TODO: Let the network quickly oscillation
+    # TODO: How to let the network emerge the behavior of the brain?
     model = RNN()
+    model.train()
 
-    writer = tf.summary.create_file_writer("./logs/")
-    for epoch in range(constants.num_epochs):
-        # TODO: Let the network quickly oscillation
-        # TODO: How to let the network emerge the behavior of the brain?
 
-        batch_loss = model.train_one_iteration()
-        mean_loss = float(np.mean(batch_loss))
-
-        with writer.as_default():
-            tf.summary.scalar("loss", mean_loss, step=epoch)
-            tf.summary.scalar("learning_rate", model.get_learning_rate(), step=epoch)
-        writer.flush()
-
-        if epoch % constants.save_epochs == 0:
-            model.save(epoch)
-
-        if epoch % constants.report_epochs == 0:
-            print("%d : loss = %f" % (epoch, mean_loss))
+def test():
+    model = RNN()
+    model.load()
+    model.evaluate()
 
 
 if __name__ == "__main__":
