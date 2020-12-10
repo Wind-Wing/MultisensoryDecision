dc32c70d3a66b546fb0544c72a116c6ee8d2f829
Wind-Wing <windwing.me@gmail.com>
2020-12-10 23:01:24 +0800

    Update network.py: add visualize and add lr&decay into dir name

--- network.py
+++ network.py
@@ -3,6 +3,7 @@ import os
 import tensorflow as tf
 import constants
 from data import DataGenerator
+import analyse
 
 
 class RNN(object):
@@ -35,12 +36,13 @@ class RNN(object):
         )
 
     def train(self, noise_ratio=None, velocity_input_delay=0):
-        sub_dir = self.get_dir(noise_ratio=noise_ratio, velocity_input_delay=velocity_input_delay)
+        sub_dir = self.get_dir(noise_ratio=noise_ratio, delay=velocity_input_delay)
         print(sub_dir)
         os.mkdir(self.ckpt_dir + sub_dir)
 
         tensor_board = tf.keras.callbacks.TensorBoard(log_dir=self.log_dir + sub_dir, update_freq=100)
         model_ckpt = tf.keras.callbacks.ModelCheckpoint(filepath=self.ckpt_dir + sub_dir + self.ckpt_name)
+        pred_visulize = tf.keras.callbacks.LambdaCallback(on_epoch_end= lambda _, __: analyse.visualize(self.model, 4))
 
         self.model.fit(
             x=self.data_generator.batch_generator(noise_ratio, velocity_input_delay),
@@ -49,13 +51,15 @@ class RNN(object):
             verbose=1,
             workers=4,
             use_multiprocessing=True,
-            callbacks=[tensor_board, model_ckpt]
+            callbacks=[tensor_board, model_ckpt, pred_visulize]
         )
 
-    def load(self, epoch, noise_ratio=None, velocity_input_delay=0):
+    def load(self, epoch, noise_ratio=None, delay=0, lr=constants.learning_rate, decay=constants.learning_rate_decay):
         ckpt_path = self.ckpt_dir + self.get_dir(
             noise_ratio=noise_ratio,
-            velocity_input_delay=velocity_input_delay
+            delay=delay,
+            lr=lr,
+            decay=decay
         )
         ckpt_name = ckpt_path + self.ckpt_name.format(epoch=epoch)
         print("Load from " + ckpt_name)
@@ -69,12 +73,14 @@ class RNN(object):
         )
         return preds
 
-    def get_dir(self, noise_ratio=None, velocity_input_delay=0):
+    def get_dir(self, noise_ratio=None, delay=0, lr=constants.learning_rate, decay=constants.learning_rate_decay):
         name = "bs" + str(constants.training_batch_size)
         if noise_ratio is not None:
             name += "_noise" + str(noise_ratio)
-        name += "_delay" + str(velocity_input_delay)
+        name += "_delay" + str(delay)
         name += "_steps" + str(constants.steps_per_epoch)
+        name += "_lr" + str(lr)
+        name += "_lrDecay" + str(decay)
         name += "/"
         return name
 
@@ -85,6 +91,3 @@ class RNN(object):
             batch_size=bs
         )
         return rnn_states
-
-
-
