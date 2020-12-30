2c12f48af70f4ef1e76b67adc50d4e3117f74de0
Zishuo Zheng <Wind-Wing>
2020-12-30 22:09:42 +0800

    noise psychophysical curve analyse

--- analyse.py
+++ analyse.py
@@ -5,6 +5,11 @@ from sklearn.decomposition import PCA
 import numpy as np
 import time
 import os
+import network
+import data
+
+
+analyse_dir = "./analyse_results/"
 
 
 def validate(model, bs=constants.training_batch_size):
@@ -15,7 +20,13 @@ def validate(model, bs=constants.training_batch_size):
     fig_dir = "./figs/" + constants.get_dir()
     if not os.path.exists(fig_dir):
         os.mkdir(fig_dir)
+
+    visualize(inputs, gts, preds, fig_dir)
+
+
+def visualize(inputs, gts, preds, prefix):
     x = range(int(inputs.shape[1]))
+    bs = int(inputs.shape[0])
     col_num = 4
     row_num = min(bs, 6)
     for i in range(row_num):
@@ -24,10 +35,10 @@ def validate(model, bs=constants.training_batch_size):
         plt.subplot(row_num, col_num, col_num * i + 2)
         plt.plot(x, inputs[i, :, 1])
         plt.subplot(row_num, col_num, col_num * i + 3)
-        plt.plot(x, gts[i, :, 0] * data_generator.normalization_factor)
+        plt.plot(x, gts[i, :, 0])
         plt.subplot(row_num, col_num, col_num * i + 4)
-        plt.plot(x, preds[i, :, 0] * data_generator.normalization_factor)
-    plt.savefig(fig_dir + str(time.time()) + ".png")
+        plt.plot(x, preds[i, :, 0])
+    plt.savefig(prefix + str(time.time()) + ".png")
     plt.clf()
 
 
@@ -56,14 +67,39 @@ def dynamic_system(model, bs):
 
 
 def delay_interval():
-    pass
+
 
 
 def noise_psychophysical_curve():
-    pass
+    bs = 1000
+    noise_ratio_list = np.arange(0, 4, 0.1)
+
+    model = build_and_load_model(noise_ratio=0.1)
+    data_generator = data.DataGenerator(bs)
+    avg_res = []
+    vote_res = []
+    for noise_ratio in noise_ratio_list:
+        print("Noise Level " + str(noise_ratio))
+        inputs, gts = data_generator.next_batch(noise_ratio=noise_ratio)
+        preds = model.predict(inputs, bs)
+        # visualize(inputs, gts, preds, analyse_dir + str(noise_ratio) + "-")
+
+        gt_direction = gts[:, -1, 0] > 0
+        avg_direction = np.mean(preds[:, :, 0], axis=1) > 0.
+        vote_direction = np.mean(preds[:, :, 0] > 0, axis=1) > 0.5
+        avg_direction_acc = np.mean(np.equal(gt_direction, avg_direction))
+        vote_direction_acc = np.mean(np.equal(gt_direction, vote_direction))
+        avg_res.append(avg_direction_acc)
+        vote_res.append(vote_direction_acc)
+
+    plt.plot(noise_ratio_list, avg_res, color="red")
+    plt.plot(noise_ratio_list, vote_res, color="green")
+    plt.savefig(analyse_dir + "noise_psychophysical_curve-" + str(time.time()) + ".png")
+    plt.clf()
 
 
 def neuron_type():
+    # TODO: response to what signal
     pass
 
 
@@ -81,3 +117,14 @@ def spike_sequence_diagram():
 
 def attractor_super_plane():
     pass
+
+
+def build_and_load_model(noise_ratio=None, delay=0):
+    ckpt_dir = constants.get_dir(noise_ratio, delay)
+    model = network.RNN()
+    model.load(ckpt_dir, constants.num_epochs)
+    return model
+
+
+if __name__ == "__main__":
+    noise_psychophysical_curve()
